45dd613e42cbbba5109d5af0695b1b6c5c8fc37f
chenguang du <duchengaung@licaigc.com>
2019-08-01 09:55:33 +0800

    tmp change

--- FutureWebsocketAPI.go
+++ FutureWebsocketAPI.go
@@ -3,7 +3,9 @@ package goghostex
 type FutureWebsocketAPI interface {
 	GetExchangeName() string
 
-	Login()
+	Login() // 开始时必须登录，除非设置的是不用登录的
+
+	Ping(pingTime int64, pongTime int64)  // 判断ping不同的时间，如果超过多长时间发送ping，如果超过多长时间没有接受到pong， 这重启。
 
 	Subscribe()
 
