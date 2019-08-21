35c494958ad846beb65fb6124b35421a34d3cca5
chenguang du <duchengaung@licaigc.com>
2019-08-21 19:37:23 +0800

    force add bitstamp test

---
+++ bitstamp/BitstampSpotRest_test.go
@@ -0,0 +1,68 @@
+package bitstamp
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+
+	. "github.com/strengthening/goghostex"
+)
+
+func TestSpot_GetTicker(t *testing.T) {
+
+	config := &APIConfig{
+		Endpoint: ENDPOINT,
+		HttpClient: &http.Client{
+			Transport: &http.Transport{
+				Proxy: func(req *http.Request) (*url.URL, error) {
+					return url.Parse("socks5://127.0.0.1:1090")
+				},
+			},
+		},
+		ApiKey:        "",
+		ApiSecretKey:  "",
+		ApiPassphrase: "",
+		Location:      time.Now().Location(),
+	}
+
+	b := New(config)
+	ticker, resp, err := b.Spot.GetTicker(CurrencyPair{BTC, USD})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Println(string(resp))
+	tickerResponse, _ := json.Marshal(*ticker)
+	fmt.Println(string(tickerResponse))
+}
+
+func TestSpot_GetDepth(t *testing.T) {
+
+	config := &APIConfig{
+		Endpoint: ENDPOINT,
+		HttpClient: &http.Client{
+			Transport: &http.Transport{
+				Proxy: func(req *http.Request) (*url.URL, error) {
+					return url.Parse("socks5://127.0.0.1:1090")
+				},
+			},
+		},
+		ApiKey:        "",
+		ApiSecretKey:  "",
+		ApiPassphrase: "",
+		Location:      time.Now().Location(),
+	}
+
+	b := New(config)
+	_, resp, err := b.Spot.GetDepth(10, CurrencyPair{BTC, USD})
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	fmt.Println(string(resp))
+}
