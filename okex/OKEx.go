961fe29fd3a2e5b88a527bb6b4014a616f889619
strengthen <ducg@foxmail.com>
2022-01-27 10:08:39 +0800

    change the okex.com to okx.com

--- okex/OKEx.go
+++ okex/OKEx.go
@@ -70,7 +70,7 @@ const (
 	XRP_USD_SWAP = "XRP-USD-SWAP"
 
 	/*Rest Endpoint*/
-	ENDPOINT              = "https://www.okex.com"
+	ENDPOINT              = "https://www.okx.com"
 	GET_ACCOUNTS          = "/api/swap/v3/accounts"
 	PLACE_ORDER           = "/api/swap/v3/order"
 	CANCEL_ORDER          = "/api/swap/v3/cancel_order/%s/%s"
