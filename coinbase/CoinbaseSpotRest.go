4591f5c24f04fa0012b6852f562fae0216ba78e7
gankthisway <gankthisway@outlook.com>
2024-05-16 19:02:53 +0800

    update the coinbase historical candles

--- coinbase/CoinbaseSpotRest.go
+++ coinbase/CoinbaseSpotRest.go
@@ -140,6 +140,70 @@ func (spot *Spot) GetKlineRecords(pair Pair, period, size, since int) ([]*Kline,
 	return GetAscKline(klines), resp, nil
 }
 
+func (spot *Spot) GetHistoricalCandles(pair Pair, period, size, since int) ([]*Kline, []byte, error) {
+	if size > 300 {
+		return nil, nil, errors.New("Can not request more than 300. ")
+	}
+
+	granularity, exist := _INERNAL_KLINE_PERIOD_CONVERTER[period]
+	if !exist {
+		return nil, nil, errors.New("The coinbase just support 1min 5min 15min 6h 1day. ")
+	}
+
+	uri := fmt.Sprintf(
+		"/products/%s/candles?",
+		pair.ToSymbol("-", true),
+	)
+
+	params := url.Values{}
+
+	if since > 0 {
+		startTimeFmt := fmt.Sprintf("%d", since)
+		if len(startTimeFmt) >= 10 {
+			startTimeFmt = startTimeFmt[0:10]
+		}
+		ts, err := strconv.ParseInt(startTimeFmt, 10, 64)
+		if err != nil {
+			return nil, nil, err
+		}
+		startTime := time.Unix(ts, 0).UTC()
+		endTime := time.Unix(ts+int64(size*granularity), 0).UTC()
+
+		params.Add("start", startTime.Format(time.RFC3339))
+		params.Add("end", endTime.Format(time.RFC3339))
+	}
+
+	params.Add("granularity", fmt.Sprintf("%d", granularity))
+	var response [][]interface{}
+	resp, err := spot.DoRequestHistorical(
+		"GET",
+		uri+params.Encode(),
+		"",
+		&response,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var klines []*Kline
+	for _, item := range response {
+		t := time.Unix(ToInt64(item[0]), 0)
+		klines = append(klines, &Kline{
+			Exchange:  COINBASE,
+			Timestamp: t.UnixNano() / int64(time.Millisecond),
+			Date:      t.In(spot.config.Location).Format(GO_BIRTHDAY),
+			Pair:      pair,
+			Open:      ToFloat64(item[3]),
+			High:      ToFloat64(item[2]),
+			Low:       ToFloat64(item[1]),
+			Close:     ToFloat64(item[4]),
+			Vol:       ToFloat64(item[5])},
+		)
+	}
+
+	return GetAscKline(klines), resp, nil
+}
+
 func (*Spot) GetTrades(pair Pair, since int64) ([]*Trade, error) {
 	panic("implement me")
 }
