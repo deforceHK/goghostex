df89e6f2effdf2c14c4969cd32f43de363d02350
strengthen <ducg@foxmail.com>
2024-10-05 16:07:13 +0800

    code review the bn ws

--- binance/BinanceWebsocket_test.go
+++ binance/BinanceWebsocket_test.go
@@ -30,9 +30,49 @@ func TestBinanceFutureWebsocket_Start(t *testing.T) {
 		Config: config,
 	}
 
-	wsBN.Start()
+	err := wsBN.Start()
+	if err != nil {
+		t.Error(err)
+		return
+	}
 
 	wsBN.Subscribe("balance")
 
 	time.Sleep(600 * time.Second)
+}
+
+// go test -v ./binance/... -count=1 -run=TestBinanceSwapMarketWebsocket
+func TestBinanceSwapMarketWebsocket(t *testing.T) {
+	var config = &APIConfig{
+		Endpoint:   ENDPOINT,
+		HttpClient: &http.Client{
+			//Transport: &http.Transport{
+			//	Proxy: func(req *http.Request) (*url.URL, error) {
+			//		return url.Parse(PROXY_URL)
+			//	},
+			//},
+		},
+		ApiKey:        SWAP_API_KEY,
+		ApiSecretKey:  SWAP_API_SECRETKEY,
+		ApiPassphrase: SWAP_API_PASSPHRASE,
+		Location:      time.Now().Location(),
+	}
+
+	var wsBN = &WSMarketUMBN{
+		&WSTradeUMBN{
+			Config: config,
+		},
+	}
+
+	err := wsBN.Start()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	time.Sleep(10 * time.Second)
+
+	wsBN.Subscribe("solusdt@depth@500ms")
+	select {
+
+	}
 }
